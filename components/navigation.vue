8713badac924715c0fd8a5d6a46bbcde66c49e4a
Kieran <mr.kieran.mcbride@googlemail.com>
2022-07-23 16:31:51 +0100

    #7 - Updated Navigation with mobile hamburger

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="relative pb-5 mb-5 border-b">
-		<div class="flex items-center justify-between text-lg">
+		<div class="flex flex-col md:flex-row items-center justify-between text-lg">
 			<nuxt-link class="font-mono font-bold text-2xl" to="/"><span>&lt;codebykieran/&gt;</span></nuxt-link>
 			<ul id="navbar" class="hidden md:flex space-x-6">
 				<nuxt-link class="nav-link" to="/">home</nuxt-link>
@@ -8,13 +8,87 @@
 				<nuxt-link class="nav-link" to="/work">work</nuxt-link>
 				<nuxt-link class="nav-link" to="/contact">contact</nuxt-link>
 			</ul>
+			<button id="menu-btn" :class="navOpen ? 'open' : ''" class="block hamburger mt-6 md:hidden focus:outline-none" @click="toggleNav">
+				<span class="hamburger-top"></span>
+				<span class="hamburger-middle"></span>
+				<span class="hamburger-bottom"></span>
+			</button>
+			<div class="md:hidden">
+				<div id="menu" :class="navOpen ? 'flex' : 'hidden'" class="absolute flex-col items-center border-2 border-yellow-600 self-end py-8 mt-10 space-y-6 font-semibold bg-white sm:w-auto sm:self-center left-0 right-0">
+					<nuxt-link class="nav-link" @click.native="toggleNav" to="/">home</nuxt-link>
+					<nuxt-link class="nav-link" @click.native="toggleNav" to="/about">about</nuxt-link>
+					<nuxt-link class="nav-link" @click.native="toggleNav" to="/work">work</nuxt-link>
+					<nuxt-link class="nav-link" @click.native="toggleNav" to="/contact">contact</nuxt-link>
+				</div>
+			</div>
 		</div>   
 	</nav>
 </template>
 
 <script>
     export default {
+		data() {
+			return {
+				navOpen: false,
+			}
+		},
+		methods: {
+			toggleNav() { 
+				this.navOpen = !this.navOpen
+			}
+		}
         
     }
 </script>
+
+<style scoped>
+
+.hamburger {
+	cursor: pointer;
+	width: 24px;
+	height: 24px;
+	transition: all 0.25s;
+	position: relative;
+}
+
+.hamburger-top,
+.hamburger-middle,
+.hamburger-bottom {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 24px;
+	height: 2px;
+	background: orange;
+	transform: rotate(0);
+	transition: all 0.5s;
+}
+
+.hamburger-middle{
+	transform: translateY(7px);
+}
+
+.hamburger-bottom{
+	transform: translateY(14px);
+}
+
+.open {
+	transform: rotate(90deg);
+	transform: translate(0px);
+}
+
+.open .hamburger-top {
+	transform: rotate(45deg) translateY(6px) translate(6px); 
+}
+
+.open .hamburger-middle {
+	display: none;
+}
+
+.open .hamburger-bottom {
+	transform: rotate(-45deg) translateY(6px) translate(-6px); 
+}
+
+
+</style>
 
