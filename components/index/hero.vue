474f7b4aefb89334417628e84748f1759c7c53d5
Kieran <mr.kieran.mcbride@googlemail.com>
2022-07-23 12:09:53 +0100

    #3 - Added hero compontent & SVG folder
    Created Hero component for homepage
    Added folder in assets for SVG storage

---
+++ components/index/hero.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col-reverse md:flex-row align-center justify-between">
+        <div class="flex flex-col space-y-6 align-center justify-center max-w-3xl">
+            <h1 class="text-6xl font-thin">Welcome,</h1>
+            <span class="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique consectetur at omnis molestiae unde possimus labore excepturi tempora quaerat, perferendis ipsum porro blanditiis, voluptates dolorum, doloribus eius eos provident consequatur! Amet suscipit minus ab voluptatum temporibus eius tenetur doloremque possimus corporis, natus molestias aliquid placeat impedit, accusamus optio fuga vitae.</span>
+            <button class="rounded-full py-3 text-white bg-gray-800 w-1/3">Take a Look</button>
+        </div>
+        <img src="../../assets/svg/my-app.svg" class="max-w-full md:max-w-xl mx-auto md:mx-0 py-12">
+    </div>
+</template>
