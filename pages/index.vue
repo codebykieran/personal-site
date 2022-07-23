f3e0b7532f646f9bd711c3954ef86eabf08765ff
Kieran <mr.kieran.mcbride@googlemail.com>
2022-07-23 08:56:00 +0100

    First Commit - Initial Setup

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- <Tutorial/> -->
+  <nav class="relative container mx-auto p-6">
+    <span class="text-lg font-semibold">&lt;codebykieran/&gt;</span>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
