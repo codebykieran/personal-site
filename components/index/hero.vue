68d63edea6c35ef34aefb89a445c68c36622b16d
Kieran <mr.kieran.mcbride@googlemail.com>
2022-10-09 15:46:23 +0100

    #11 - General Updates
    - Updated styling for blinking cursor effect
    - Added styling for nav link underlines
    - Added SVG's
    - Updated Hero component styling made use of new svg and button partial
    - Added tailwind config file

--- components/index/hero.vue
+++ components/index/hero.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex flex-col-reverse md:flex-row align-center justify-between">
-        <div class="flex flex-col space-y-6 align-center justify-center md:w-9/12 lg:w-6/12">
-            <h1 class="font-mono text-4xl md:text-5xl lg:text-6xl font-light">Full-Stack Developer</h1>
+        <div class="flex flex-col space-y-6 align-center justify-center md:w-6/12 lg:w-6/12">
+            <h1 class="font-mono text-4xl md:text-4xl lg:text-6xl font-light">Front-End Developer</h1>
             <span class="text-base lg:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore deleniti, facilis laborum dolor molestiae ratione ipsa. Natus ullam odit delectus tempore, veritatis enim, quisquam libero dolorum ad consequatur minus est, commodi sunt earum quia nulla porro officiis perspiciatis eligendi? Officia molestiae inventore qui distinctio? Praesentium id asperiores nulla pariatur dolorem neque aperiam ipsam, saepe accusamus odio reprehenderit quas eligendi?</span>
-            <button class="rounded-full py-3 text-white bg-gray-800 w-1/3 min-w-sm">Tell Me More</button>
+            <partials-button url="/about" text="Discover More"></partials-button>
         </div>
-        <img src="../../assets/svg/my-app.svg" class="w-10/12 md:w-3/12 lg:w-5/12 mx-auto md:mx-0 py-12">
+        <img src="../../assets/svg/programming.svg" class="w-10/12 md:w-4/12 lg:w-5/12 mx-auto md:mx-0 py-12">
     </div>
 </template>
 
@@ -15,13 +15,7 @@ h1::after {
     content: '_';
     margin-left: .5rem;
     animation: 
-        blink-caret .5s step-end infinite;
-}
-
-/* The blinking cursor effect */
-@keyframes blink-caret {
-  from, to { color: transparent }
-  50% { color: orange }
+        blinking .5s step-end infinite;
 }
 
 </style>
