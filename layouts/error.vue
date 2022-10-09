654318024cec4d0b9005e8f4fa00902485e26fb6
Kieran <mr.kieran.mcbride@googlemail.com>
2022-10-09 15:43:31 +0100

    #10 - Updated the Error layout
    - Added SVG for 404 error
    - Updated content and made use of button partial

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,25 @@
 <template>
+  <div>
     <div>
-        <h1 v-if="error.statusCode === 404">Page not found</h1>
-        <h1 v-else>An error occurred</h1>
-        <NuxtLink to="/">Home page</NuxtLink>
+      <div v-if="error.statusCode === 404" class="container mx-auto flex flex-col items-center justify-center py-10">
+        <img class="w-25 md:w-1/2" src="../assets/svg/404.svg" />
+        <h1 class="font-mono text-xl font-bold my-7">
+          This page was not found.
+        </h1>
+        <partials-button url="/" text="Take Me Home"></partials-button>
+      </div>
+      <div v-else class="container mx-auto flex flex-col items-center justify-center py-10">
+        <img class="w-25 md:w-1/2" src="../assets/svg/error.svg" />
+        <h1>An unexpected error occured!</h1>
+        <partials-button url="/" text="Take Me Home"></partials-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: ['error'],
-    layout: 'error' // you can set a custom layout for the error page
-  }
+export default {
+  props: ["error"],
+  layout: "error", // you can set a custom layout for the error page
+};
 </script>
