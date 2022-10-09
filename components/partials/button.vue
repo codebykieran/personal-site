cb505b28a56622e8dc7355ef159477f23754f171
Kieran <mr.kieran.mcbride@googlemail.com>
2022-10-09 15:34:35 +0100

    #9 - Added Partial component for Buttons
    - Reusable button component which accepts the props 'url' and 'text'

---
+++ components/partials/button.vue
@@ -0,0 +1,18 @@
+<template>
+    <nuxt-link :to="url">
+        <button class="rounded-full py-3 px-8 text-white bg-gray-800 hover:bg-gray-900 w-full md:max-w-xs">{{ text }}</button>
+    </nuxt-link>
+</template>
+
+<script>
+
+export default {
+
+    props: { 
+        url: String,
+        text: String
+    }
+
+}
+
+</script>
